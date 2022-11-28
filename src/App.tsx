c2d4b9b48a74ab54e549e432851ece8b87c3889e
SVA2021 <sofronov.vit@gmail.com>
2022-11-28 17:25:30 +0400

    base routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {Layout} from './layout';
+import {Home, Todo} from './pages';
+import {TodoProvider} from './providers/todo.context';
 
 export const App = () => {
   return (
     <div className="container">
-      initial commit
+      <TodoProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/todo/:id' element={<Todo />} />
+          </Route>
+          <Route path='*' element={<Layout />} ></Route>
+        </Routes>
+      </TodoProvider>
     </div>
   );
 };
