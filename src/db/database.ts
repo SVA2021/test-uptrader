9976ec8e051ffe466de50cef4db4982f51679ec3
SVA2021 <sofronov.vit@gmail.com>
2022-11-30 10:57:24 +0400

    add new tasks to db

--- src/db/database.ts
+++ src/db/database.ts
@@ -37,7 +37,63 @@ export const ProjectsFake: ProjectI[] = [
                     },
                 ],
 
-            }
+            },
+            {
+                id: 1,
+                title: 'second task',
+                description: 'description of first task. i think it will not be too long',
+                initialDate: '2022-11-27',
+                expirationDate: '2022-11-30',
+                priority: 'high',
+                files: ['filepath2',],
+                status: 'Done',
+                subTask: [
+                    {
+                        isFinished: false,
+                        description: 'subtask description 1',
+                    },
+                    {
+                        isFinished: true,
+                        description: 'subtask description 2',
+                    },
+                ],
+                comments: [
+                    {
+                        id: 11,
+                        name: 'comment 1111',
+                        subComments: [],
+                    },
+                ],
+
+            },
+            {
+                id: 2,
+                title: 'another task',
+                description: 'description of first task. i think it will not be too long',
+                initialDate: '2022-11-27',
+                expirationDate: '2022-11-30',
+                priority: 'low',
+                files: ['filepath2',],
+                status: 'Queue',
+                subTask: [
+                    {
+                        isFinished: false,
+                        description: 'subtask description 1',
+                    },
+                    {
+                        isFinished: true,
+                        description: 'subtask description 2',
+                    },
+                ],
+                comments: [
+                    {
+                        id: 11,
+                        name: 'comment 1111',
+                        subComments: [],
+                    },
+                ],
+
+            },
         ],
     },
     {
